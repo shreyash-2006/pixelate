ee06851464ce932b82d4e1a32a63f67dacd4b20b
shreyash-2006 <shreyashbhor2006@gmail.com>
2026-04-05 16:43:48 +0530

    soultion_final

--- bonus_task/template.cpp
+++ bonus_task/template.cpp
@@ -14,9 +14,17 @@ int main()
     }
 
     // TODO: decode and print the hidden message.
-    
-    for (int i=0; i<=img.cols();i++){
-        swap(a[i],a[img.cols()-i-1]);
+    for (int j=0;i<=ing,rows();j++){
+        if (j%2==0){
+            for (int i=0; i<=img.cols();i++){
+        swap(img[i],a[img.cols()-i-1]);
     }
+        }
+        else{
+            for (int k=img.cols(); k>=0();k--){
+        swap(a[img.cols()-k-1],img[k]);
+        }
+    }
+    
     return 0;
 }
